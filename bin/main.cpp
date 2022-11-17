48c5dd8703d09f07cec53ba461e8c7a755df1db1
Alexander  Khvastunov <spbdeveloper@yandex.ru>
2022-11-17 20:52:15 +0300

    Update main.cpp

--- bin/main.cpp
+++ bin/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
     ArgumentParser::ArgParser parser("Program");
     parser.AddIntArgument("N").MultiValue(1).Positional().StoreValues(values);
     parser.AddFlag("sum", "add args").StoreValue(opt.sum);
-    parser.AddFlag("mult", "multiply args").StoreValue(opt.sum);
+    parser.AddFlag("mult", "multiply args").StoreValue(opt.mult);
     parser.AddHelp('h', "help", "Program accumulate arguments");
 
     if(!parser.Parse(argc, argv)) {
